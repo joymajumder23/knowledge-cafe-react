f65c6525ea422c11ff174fe84219858095aeb2cb
joymajumder23 <joymajumderjishu@gmail.com>
2024-03-16 00:05:21 +0600

    Blogs and Bookmarks added

---
+++ src/components/Bookmarks/Bookmarks.jsx
@@ -0,0 +1,9 @@
+const Bookmarks = () => {
+    return (
+        <div className="3/2">
+            <h1>Bookmarks</h1>
+        </div>
+    );
+};
+
+export default Bookmarks;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import Profile from '../assets/images/profile.png'
 const Header = () => {
     return (
-        <div className='flex justify-between p-4'>
+        <div className='flex justify-between p-4 mx-4 border-b-2'>
           <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>
           <img src={Profile} alt="" /> 
         </div>
