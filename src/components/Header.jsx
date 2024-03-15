e5e4b05d0f5a952447f279063c174e81e4689bdb
joymajumder23 <joymajumderjishu@gmail.com>
2024-03-15 23:14:29 +0600

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
+import Profile from '../assets/images/profile.png'
 const Header = () => {
     return (
-        <div>
-          <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>  
+        <div className='flex justify-between p-4'>
+          <h1 className='text-3xl font-bold'>Knowledge Cafe</h1>
+          <img src={Profile} alt="" /> 
         </div>
     );
 };
